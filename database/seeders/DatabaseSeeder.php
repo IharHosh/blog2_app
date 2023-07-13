aa6cbb7cf760e1228560ded3328114973a3a414e
Ihar Hosh <igor.goshko007@gmail.com>
2023-07-13 22:42:06 +0200

    fourth_commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,17 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Cinema;
+use App\Models\CinemaMovie;
+use App\Models\Director;
+use App\Models\Film;
+use App\Models\Movie;
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User1;
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +29,30 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Заполним данными табличку directors
+        // Director::factory(30)->create();
+
+        // Заполним данными табличку films
+        // Film::factory(30)->create();
+
+        // Заполним данными табличку authors
+//            Author::factory(5)->create();
+
+        // Заполним данными табличку books
+//        Book::factory(50)->create();
+
+        // Заполним данными табличку cinema и movie
+//        Cinema::factory(8)->create();
+//        Movie::factory(30)->create();
+
+        // Заполним данными табличку cinema_movie
+//        CinemaMovie::factory(50)->create();
+
+//        User1::factory(5)->create();
+//        Project::factory(10)->create();
+//        Task::factory(30)->create();
+
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,102 @@ use Illuminate\Support\Facades\Route;
     Route::post('/blog/{post}/like', [BlogController::class, 'like'])->name('blog.like');
 
 
+// выведем все tasks ч-з табличку projects
+Route::get('/tasks', function () {
+    $users = \App\Models\User1::all();
+    foreach ($users as $user) {
+        echo 'User name: ' . $user['name'] . '<br>';
+        echo '<b>Tasks:</b><br>';
+        foreach ($user->tasks as $task) {
+            echo $task['name'] . '<br>';
+        }
+        echo '_________________________<br>';
+//        dd($user1s->toArray());
+    }
+});
+
+
+//// выведем все кинотеатры и фильмы пренадл. им
+//Route::get('/films', function () {
+//    $movies = \App\Models\Movie::all();
+//    foreach ($movies as $movie) {
+//        echo 'Movie name: ' . $movie['name'] . '<br>';
+//        echo '<b>Cinema:</b><br>';
+//        foreach ($movie->cinemas as $cinema) {
+//            echo $cinema['name'] . '<br>';
+//        }
+//        echo '_________________________<br>';
+//    }
+//});
+
+
+
+
+    //    // выведем все кинотеатры и фильмы пренадл. им
+//    Route::get('/cinemas', function () {
+//        $cinemas = \App\Models\Cinema::all();
+//        foreach ($cinemas as $cinema) {
+//            echo 'Cinema: ' . $cinema['name'] . '<br>';
+//            echo '<b>Movies:</b><br>';
+//            foreach ($cinema->movies as $movie) {
+//                echo $movie['name'] . '<br>';
+//            }
+//            echo '_________________________<br>';
+//        }
+//    });
+
+
+
+
+
+
+
+
+
+
+
+        // выводит книги и их авторов
+//        Route::get('/books', function () {
+//        $books = \App\Models\Book::all();
+//        foreach ($books as $book){
+//            echo 'Book title: '.$book['title'].'<br>';
+//            echo '<b>Author:</b>'.$book->author['name'].'<br>';
+//
+//            echo '_________________________<br>';
+//        }
+//    });
+
+
+    // выводит авторов и все их книги
+//    Route::get('/authors', function () {
+//        $authors = \App\Models\Author::all();
+//        foreach ($authors as $author){
+//            echo 'Author name: '.$author['name'].'<br>';
+//            echo '<b>Author\'s books:</b><br>';
+//            foreach ($author->books as $book){
+//                echo $book['title'].'<br>';
+//            }
+//            echo '_________________________<br>';
+//        }
+//
+//    });
+
+
+//    Route::get('/films', function () {
+////        $directors = \App\Models\Director::all();
+//        $films = \App\Models\Film::all();
+//
+////        foreach ($directors as $director) {
+////            echo 'Director name: '.$director['name'].'<br>';
+////            echo 'Film: '.$director->film['name'].'<br>';
+////            echo '_________________________<br>';
+////        }
+//        foreach ($films as $film) {
+//            echo 'Film name: '.$film['name'].'<br>';
+//            echo 'Director: '.$film->director['name'].'<br>';
+//            echo '_________________________<br>';
+//        }
+//    });
 //    Route::resource('/posts/{post}/comments', CommentController::class)->only('index', 'show');
 // создает все маршруты для всех методов (вышесозданных)
 // если добавить ->only в скобках параметры которые будут задействованы
